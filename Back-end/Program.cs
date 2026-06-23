c6aeec8b85a598ed8342c7498a9b9ee2b61d00a1
Kamile Alves da Silva <kamilealvss@gmail.com>
2026-06-22 23:02:47 -0300

    Update CORS policy to allow additional origin

--- Back-end/Program.cs
+++ Back-end/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("PermitirNextJs", policy =>
     {
-        policy.WithOrigins("http://localhost:3000")
+        policy.WithOrigins("http://localhost:3000","https://calculadora-de-investimento-nu.vercel.app/")
               .AllowAnyHeader()
               .AllowAnyMethod();
     });
